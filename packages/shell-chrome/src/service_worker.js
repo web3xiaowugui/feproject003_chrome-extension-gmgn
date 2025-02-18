6d4b96188a6b5f1638e2011f83d8dfeaf7d8f1b8
web3xiaowugui <web3xiaowugui@gmail.com>
2025-02-18 16:24:39 +0800

    收集gmgn所有的api

--- packages/shell-chrome/src/service_worker.js
+++ packages/shell-chrome/src/service_worker.js
@@ -1,4 +1,3 @@
-
 // console.log("Colibri service_worker.js")
 
 import {
@@ -9,7 +8,7 @@ import {
 
 // 同步数据
 function setStore(k, v) {
-    chrome.storage.local.set({ [k]: v });
+    chrome.storage.local.set({[k]: v});
 }
 
 // 获取数据
@@ -23,12 +22,45 @@ function getStore(key, defaultValue = null) {
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+    chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true});
 });
 
+
+/*
+例子
+{
+    "type": "colibri:notice:type",
+    "to": "colibri:notice:background",
+    "key": "colibri:notice:global-switch",
+    "value": true
+}
+
+{
+    "type": "colibri:notice:type",
+    "to": "colibri:notice:background",
+    "key": "colibri:notice:hit-rate",
+    "value": {
+        "url": "https://gmgn.ai/_next/static/css/beac95bcec9e3f3f.css",
+        "match_url": "https://gmgn.ai",
+        "method": "ANY"
+    }
+}
+
+{
+    "type": "colibri:notice:type",
+    "to": "colibri:notice:background",
+    "key": "colibri:notice:hit-rate",
+    "value": {
+        "url": "https://gmgn.ai/defi/quotation/v1/follow/sol/following_wallets?network=sol...",
+        "match_url": "https://gmgn.ai",
+        "method": "GET"
+    }
+}
+*/
 // 接收content 传来的信息
 chrome.runtime.onMessage.addListener((msg) => {
     if (msg.type === Notice.TYPE && msg.to === Notice.TO_BACKGROUND) {
+
         // 判断启用状态
         if (msg.key === NoticeKey.GLOBAL_SWITCH) {
             chrome.action.setIcon({
@@ -37,6 +69,24 @@ chrome.runtime.onMessage.addListener((msg) => {
         }
         // 监听命中率
         if (msg.key === NoticeKey.HIT_RATE) {
+            // 收集gmgn所有url
+            const baseXwgUrl = "http://localhost:9001/gmgn_data_collector"
+            fetch(baseXwgUrl + "/api/insertOrIgnore", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(msg.value),
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            }).then(data => {
+            }).catch(error => {
+                console.error("Error", error)
+            });
+
             // 获取本地拦截列表
             getStore(StorageKey.INTERCEPT_LIST, [])
                 .then(interceptList => {
@@ -60,9 +110,9 @@ chrome.runtime.onMessage.addListener((msg) => {
                     setStore(StorageKey.INTERCEPT_LIST, interceptList);
 
                     // 更新徽章 显示命中率
-                    chrome.action.setBadgeBackgroundColor({ color: "#006d75" });
-                    if (counter) chrome.action.setBadgeText({ text: `+${counter}` });
-                    else chrome.action.setBadgeText({ text: "" });
+                    chrome.action.setBadgeBackgroundColor({color: "#006d75"});
+                    if (counter) chrome.action.setBadgeText({text: `+${counter}`});
+                    else chrome.action.setBadgeText({text: ""});
                 })
         }
     }
